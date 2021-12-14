ee226714cdee4004b54020bfe0d44a8af90ab429
yooja <wogud758787@naver.com>
2021-12-15 00:53:21 +0900

    ing

--- sever.c
+++ sever.c
@@ -6,7 +6,63 @@
 /*   By: jayoo <jayoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 16:56:03 by jayoo             #+#    #+#             */
-/*   Updated: 2021/12/13 16:56:07 by jayoo            ###   ########.fr       */
+/*   Updated: 2021/12/14 23:28:35 by jayoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+////////////**1**//////////////
+// void sig_handler_usr1()
+// {
+// 	printf("catched sigusr1\n");
+// 	return ;
+// }
+
+// void sig_handler_usr2()
+// {
+// 	printf("catched sigusr2\n");
+// 	return ;
+// }
+
+// int main()
+// {
+// 	signal(SIGUSR1, sig_handler_usr1);
+// 	signal(SIGUSR2, sig_handler_usr2);
+
+// 	while(1) sleep(1);
+// 	return 1;
+// }
+////////////**1**//////////////
+
+void	handler(int signo, siginfo_t *info, void *context)
+{
+	if (signo == SIGUSR1)
+		write(1, "1", 1);
+    else if (signo == SIGUSR2)
+		write(1, "0", 1);
+}
+
+int		main(void)
+{
+	struct sigaction act1;
+
+	act1.sa_sigaction = handler;
+	act1.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &act1, NULL) != 0)
+	{
+		printf("Sigaction Error");
+		exit(1);
+	}
+	if (sigaction(SIGUSR2, &act1, NULL) != 0)
+	{
+		printf("Sigaction Error");
+		exit(1);
+	}
+	while (1)
+		;
+	return (0);
+}
--- test.c
+++ test.c
@@ -1,20 +1,87 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void sig_handler()
-{
-	printf("catched sigusr1\n");
-	return ;
-}
+////////////**1**//////////////
+// int main(int argc, char *argv[])
+// {
+// 	if (argc < 3)
+// 		printf("SIGNUM please\n");
+// 	else
+// 	{
+// 		if (atoi(argv[2]) == 1)
+// 			kill(atoi(argv[1]), SIGUSR1);
+// 		else if (atoi(argv[2]) == 2)
+// 			kill(atoi(argv[1]), SIGUSR2);
+// 	}
+// 	return 1;
+// }
+////////////**1**//////////////
 
-int main()
+void	post(int pid, int idx)
 {
-	if (signal(SIGUSR1, sig_handler) == SIG_ERR)
+	if (idx == 1)
 	{
-		perror("SIGUSR1 ERROR");
+		printf("1");
+		kill(pid, SIGUSR1);
 	}
-	while(1) sleep(1);
-	return 1;
+	else if (idx == 0)
+	{
+		printf("0");
+		kill(pid, SIGUSR2);
+	}
+	else
+	{
+		printf("\n");
+		kill(pid, 0);
+	}
+	usleep(1000); //0.001초  대기 // 대기를 안건다면 무슨 문제가 일어날까
 }
 
+void	conv_and_post(int pid, int c, int num)
+{
+	if (!c)
+	{
+		while (num != 8)
+		{
+			post(pid, 0);
+			num++;
+		}
+		return ;
+	}
+	else
+	{
+		conv_and_post(pid, c / 2, ++num);
+		post(pid, c % 2);
+	}
+}
+
+void	post_office(int pid, char *str)
+{
+	int		c;
+	int		num;
+	int		idx;
+
+	c = 0;
+	idx = -1;
+	while (str[++idx])
+	{
+		num = 0;
+		c = (int)str[idx];
+		conv_and_post(pid, c, num); //계속 2로 나누고 그 나머지를 차례로 보낸다.
+		post(pid, -1); //1 or 0을 각각 USR1 USR2 로 보낸다.
+	}
+}
+
+int		main(int argc, char *argv[])
+{
+	if (argc != 3)
+		return (0);
+	else
+	{
+		printf("PID : %s, Massage : %s\n", argv[1], argv[2]);
+		post_office(atoi(argv[1]), argv[2]);
+	}
+	return(0);
+}
